a19ca821d6537477e6b54b0a94f6bcd979551c2a
Akram Ghaseminia <akram.ghaseminia@snapp.cab>
2020-01-04 16:50:03 +0330

    - add post, delete and patch sample request

--- app/src/main/java/alirezat775/app/networkmonitor/MainActivity.kt
+++ app/src/main/java/alirezat775/app/networkmonitor/MainActivity.kt
@@ -5,11 +5,17 @@ import alirezat775.library.networkmonitor.core.NetworkMonitorInterceptor
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import okhttp3.*
+import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private var networkMonitor = NetworkMonitor(this)
+    private val client by lazy { OkHttpClient.Builder()
+            .addInterceptor(NetworkMonitorInterceptor())
+            .build()
+    }
+    private lateinit var jsonObject:JSONObject
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,13 +25,21 @@ class MainActivity : AppCompatActivity() {
         for (i in 0..10) {
             okHttpGet(i)
         }
+        jsonObject = JSONObject()
+        jsonObject.put("id", 1)
+        jsonObject.put("title", "foo")
+        jsonObject.put("body", "bar")
+        jsonObject.put("userId", 1)
+
+        okHttpPost(jsonObject.toString())
+        okHttpDelete()
+
+        jsonObject = JSONObject()
+        jsonObject.put("title", "foo")
+        okHttpPatch(jsonObject.toString())
     }
 
     private fun okHttpGet(i: Int) {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(NetworkMonitorInterceptor())
-            .build()
-
         val request: Request = Request.Builder()
             .url("https://jsonplaceholder.typicode.com/todos/$i")
             .build()
@@ -40,6 +54,61 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun okHttpPost(body: String){
+
+        val body = jsonObject.toString()
+        val requestBody: RequestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), body)
+        val request:Request = Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts")
+                .post(requestBody)
+                .build()
+
+        client.newCall(request).enqueue(object :Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+            }
+    })
+    }
+
+    private fun okHttpDelete() {
+        val request: Request = Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .delete()
+                .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+            }
+        })
+    }
+
+    private fun okHttpPatch(body: String){
+        val requestBody: RequestBody = RequestBody.create(MediaType.parse("application/json; charset=utf-8"), body)
+        val request :Request = Request.Builder()
+                .url("https://jsonplaceholder.typicode.com/posts/1")
+                .patch(requestBody)
+                .build()
+
+        client.newCall(request).enqueue(object :Callback {
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+            }
+
+        })
+    }
     override fun onDestroy() {
         super.onDestroy()
         networkMonitor.unRegister()
