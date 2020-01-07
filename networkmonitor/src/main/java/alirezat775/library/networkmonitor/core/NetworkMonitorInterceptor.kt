bf87298c829221e0822f57871f4ed514d2314604
Alireza Tizfahm fard <alirezat775@gmail.com>
2020-01-07 11:38:13 +0330

    request body convert to string

--- networkmonitor/src/main/java/alirezat775/library/networkmonitor/core/NetworkMonitorInterceptor.kt
+++ networkmonitor/src/main/java/alirezat775/library/networkmonitor/core/NetworkMonitorInterceptor.kt
@@ -1,5 +1,7 @@
 package alirezat775.library.networkmonitor.core
 
+import alirezat775.library.networkmonitor.OkHttpHelper
+import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.util.*
@@ -14,25 +16,22 @@ class NetworkMonitorInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val rq = chain.request()
-        val rqModel =
-            RequestNetworkModel(
-                rq.url().host() + rq.url().encodedPath(),
-                rq.method(),
-                rq.headers(),
-                rq.body()
-            )
+        val rqModel = RequestNetworkModel(
+            rq.url().host() + rq.url().encodedPath(),
+            rq.method(),
+            rq.headers(),
+            OkHttpHelper.requestToString(rq)
+        )
         val rs = chain.proceed(rq)
-        val rsModel =
-            ResponseNetworkModel(
-                rs.code(),
-                rs.message(),
-                rs.headers(),
-                rs.body()?.string()
-            )
-        if (NetworkLogging.list.size > 30) NetworkLogging.clear()
-
-        val uuid = UUID.randomUUID().toString()
+        val rsModel = ResponseNetworkModel(
+            rs.code(),
+            rs.message(),
+            rs.headers(),
+            rs.body()?.string()
+        )
+        val uuid = UUID.randomUUID().toString() + System.currentTimeMillis()
         NetworkLogging.list.add(NetworkModel(uuid, rqModel, rsModel))
+        Log.d("TAG_TEST", rqModel.method)
         return rs
     }
 }
